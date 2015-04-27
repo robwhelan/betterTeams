1fa7b69a1f2ecc2b51514c419ad29cf3900c6d64
robwhelan <whelan@gmail.com>
2015-04-27 13:45:56 -0400

    added mailer

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -1,10 +1,11 @@
 class UserNotifier < ActionMailer::Base
   default from: "whelan@gmail.com"
   
-  def send_follower_improvement_plan_notice(follower, improvement_plan)
+  def send_follower_improvement_plan_notice(follower, initiator, improvement_plan)
       @follower = follower
+      @initiator = initiator
       mail( :to => @follower.email,
-      :subject => "You've been invited to participate in an improvement plan!" )
+      :subject => "You have been invited to participate in an improvement plan!" )
       @params = '?user=' + @follower.id.to_s + '&improvement_plan=' + improvement_plan.id.to_s
   end
     
